5fc341746788de969d0b4dd86f687c6cf33b5e9b
jimin <pjimin8073@gmail.com>
2025-03-10 01:24:05 +0900

    스케쥴러, btcss

--- app.py
+++ app.py
@@ -86,8 +86,7 @@ def update_stock_prices():
 
 # 🕒 스케줄러 설정: 10초마다 실행
 scheduler = BackgroundScheduler()
-scheduler.add_job(update_stock_prices, "interval", seconds=100000)
-#scheduler.add_job(update_stock_prices, "cron", hour='20,21,22', minute='*/1', second='1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59')#test
+scheduler.add_job(update_stock_prices, "cron", hour='9-23', minute='*/10', second='30')#test
 scheduler.start()
 
 
